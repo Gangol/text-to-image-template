907702b930f506b36ff1cf891183026a1c54f231
Gangol <gangol@gmail.com>
2025-08-07 20:56:43 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "cyberpunk yeti",
     };
 
     const response = await env.AI.run(
